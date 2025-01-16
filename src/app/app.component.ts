6b4c216e761096c5f2bf099c9bbca16c7c84d78b
Khushpreet <khushpreetsingh2047.com>
2025-01-16 17:20:28 +0530

    made some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,85 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,HomeComponent,AboutComponent,ProjectsComponent,ContactComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent{
+  title = 'Khushpreet-Portfolio';
+
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+  gsap.from("#nav", {y: -100, duration: 2,opacity: 0})
+  gsap.from(".image",{x: 100, duration: 2,opacity: 0})
+  var tl = gsap.timeline();
+  tl.from(".home .text h1", {x: -100, duration: 1,opacity: 0});
+  tl.from(".home .text p", {y: 50, duration: 1,opacity: 0});
+  tl.from(".home .text button", {opacity: 0, duration: 1});
+
+   //about
+gsap.from('.about .text p,.about .text button', {
+  y: 80,opacity: 0,duration: 1.5,
+  scrollTrigger: {
+      trigger: '.about .text',
+    
+      scroller: 'body',
+      start: 'top 65%',
+  }
+
+})
+
+gsap.from('.about li', {
+  x: 80,opacity: 0,duration: 1, stagger: .3,
+  scrollTrigger: {
+      trigger: '.about .text',
+    
+      scroller: 'body',
+      start: 'top 65%',
+  }
+
+})
+
+//project
+
+gsap.from('.project img', {
+  x:-80,opacity: 0,duration: 1.5, stagger: .5,
+  scrollTrigger: {
+      trigger: '.project img',
+  
+      scroller: 'body',
+      start: 'top 65%',
+  }
+})
+
+//contact
+
+gsap.from('.contact .form .form-control,.contact .form button,.contact .info p, .contact .info li', {
+  y:80,opacity: 0,duration: .5, stagger: .2,
+  scrollTrigger: {
+      trigger: '.contact h1',
+      scroller: 'body',
+      start: 'top 65%',
+  }
+})
+
+
+  }
+
+  
+
 }
+
+
